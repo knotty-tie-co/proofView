43dd22ff126c52ddb6f8480b2e3e1b2bcdaa7e82
Mark Johnson <markjohnson303@gmail.com>
2016-08-31 11:19:24 -0600

    fixing proof timestamps

--- app/models/Proof.js
+++ app/models/Proof.js
@@ -5,9 +5,8 @@ var mongoose = require('mongoose');
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Proof', {
-    created: {type: Date, default: Date.now()},
+    created: {type: Date, default: Date.now},
     deal: {type: Schema.Types.ObjectId, ref: 'Deal'},
     image: {type: String, default: ''}
-});
-
-var i = 1;
+	}
+);
