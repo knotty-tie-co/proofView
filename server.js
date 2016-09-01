27077215363b65c38e9c68c28261cb89c854f622
Mark Johnson <markjohnson303@gmail.com>
2016-09-01 08:38:37 -0600

    more heroku fixes

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var methodOverride = require('method-override');
 
 // configuration ===========================================
 
-mongoose.connect('mongodb://localhost/proofView');
+mongoose.connect(
+	process.env.MONGODB_URI ||
+	process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/proofView');
 	
 // config files
 var db = require('./config/db');
