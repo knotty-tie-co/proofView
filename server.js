ba523658bb4576495e58b24d0f17ffd3debd06e2
Mark Johnson <markjohnson303@gmail.com>
2016-08-31 21:52:29 -0600

    udate server for heroku

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ require('./app/routes')(app); // pass our application into our routes
 require('./app/models/Deal');
 
 // start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Express server is up and running on http://localhost:8080/');
+});			// shoutout to the user
 exports = module.exports = app; 						// expose app
 
